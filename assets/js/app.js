90aaa64004566a2a2eee0d1a9f4dd6e3949a4eb0
Jose Antonio Rodriguez Rivera <joserdz052@outlook.com>
2022-02-06 11:19:02 -0600

    Agrego la primera version de la table

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import vuetify from '../plugins/vuetify'
 import App from './components/App';
 import '../css/app.css';
 
 new Vue({
     el: '#app',
+    vuetify,
     render: h => h(App)
 });
--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -1,34 +1,67 @@
 <template>
-  <div>
-    <h2 class="center">My Application</h2>
-    <div v-text="message"></div>
-    {{ message }}
-    <ul>
-      <li :key="word.id" v-for="word in words">{{ word }}</li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" style="padding-top: 50px">
+        <v-simple-table dark>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in datos" :key="item.name">
+                <td>{{ item.name }}</td>
+                <td>{{ item.lastname }}</td>
+                <td>{{ item.age }}</td>
+                <td>
+                  <v-icon class="icono" title="Editar" v-on:click="editar"
+                    >edit</v-icon
+                  >
+                  <v-icon
+                    class="icono"
+                    title="Eliminar"
+                    v-on:click="eliminar(item)"
+                    >delete</v-icon
+                  >
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      message: "A list of words",
-      words: []
-    };
+  name: "App",
+  props: {},
+  data: () => ({
+    datos: { 0: { id: "0", name: "jose", lastname: "rodriguez", age: "16" } , 1: { id: "0", name: "jose", lastname: "rodriguez", age: "16" } },
+  }),
+  methods: {
+    editar: function () {
+      alert("Hola");
+    },
+    eliminar: function (item) {
+      console.log(item);
+    },
   },
-  mounted() {
-
-    let el = document.querySelector("div[data-words]");
-    let mywords = el.dataset.words.split(",");
-
-    this.words.push.apply(this.words, mywords);
-  }
 };
 </script>
-
-<style>
-.center {
+<style scoped>
+* {
   text-align: center;
 }
-</style>    
+.icono {
+  margin: 0px 10px;
+}
+.icono:hover {
+  color: rgb(99, 99, 99);
+}
+</style>
